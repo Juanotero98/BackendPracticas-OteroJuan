016503807c25cec313c845981f584ec8f0b16a35
Juanotero98 <oterojuan1998@gmail.com>
2023-12-21 18:58:21 -0300

    Practica integradora 1

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,67 +2,122 @@ const { Router } = require('express')
 const { usersModel } = require('../models/users.model')
 
 const router = Router()
-
-
-
-
+const arrayUsuarios = [
+    {id:1, nombre:'Nombre 1', apellido:'Apellido 1', genero: 'F'},
+    {id:2, nombre:'Nombre 2', apellido:'Apellido 2', genero: 'F'},
+    {id:3, nombre:'Nombre 3', apellido:'Apellido 3', genero: 'M'},
+    {id:4, nombre:'Nombre 4', apellido:'Apellido 4', genero: 'F'},
+    {id:5, nombre:'Nombre 5', apellido:'Apellido 5', genero: 'M'},
+    {id:6, nombre:'Nombre 6', apellido:'Apellido 6', genero: 'M'},
+    {id:7, nombre:'Nombre 7', apellido:'Apellido 7', genero: 'F'},
+    {id:8, nombre:'Nombre 8', apellido:'Apellido 8', genero: 'M'},
+]
 
 // configuración 
 // GET localhost:8080 /api/users /
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     try {
-        const users = await usersModel.find({})
-    res.send(users)
-        
+        const users = await usersModel.find({});
+        res.send(users);
     } catch (error) {
-        console.log('error')
+        console.error(error);
+        
+        res.status(500).send({
+            status: 'error',
+            message: 'Error al procesar la solicitud',
+            error: error.message
+        });
     }
-    
-    
-})
+});
+
 // POST localhost:8080  /api/users /
-router.post('/', async (req, res) =>{
+router.post('/', async (req, res) => {
     try {
-        const {first_name, last_name, email} = req.body
-   const result = await usersModel.create({
-    first_name,
-    last_name,
-    email
-   })
+        const { first_name, last_name, email } = req.body;
+        const result = await usersModel.create({
+            first_name,
+            last_name,
+            email
+        });
 
-   console.log(first_name, last_name, email)
-    res.status(201).send({ 
-        status:'success'
-        
-    })
+        console.log(first_name, last_name, email);
+        res.status(201).send({
+            status: 'success',
+            payload: result  
+        });
     } catch (error) {
-        console.log(error)
-        payload: result
+        console.log(error);
+        
+        res.status(500).send({
+            status: 'error',
+            message: 'Error al procesar la solicitud',
+            error: error.message
+        });
     }
-   
-})
+});
+
 // PUT localhost:8080  /api/users /:uid
-router.put('/:uid',  async (req, res) =>{
-
-    const { uid } = req.params
-    const userToReplace = req.body
-    // venga el id
-   const result = await usersModel.updateOne({_id: uid}, userToReplace)
-
-    res.status(201).send({ 
-        status: 'success',
-        payload: result
-    })
-})
+router.put('/:uid', async (req, res) => {
+    try {
+        const { uid } = req.params;
+        const userToReplace = req.body;
+        if (!uid) {
+            return res.status(400).send({
+                status: 'error',
+                message: 'ID de usuario no proporcionado'
+            });
+        }
+        const result = await usersModel.findByIdAndUpdate(uid, userToReplace, { new: true });
+        if (!result) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'Usuario no encontrado'
+            });
+        }
+        res.status(200).send({
+            status: 'success',
+            payload: result
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            status: 'error',
+            message: 'Error al procesar la solicitud',
+            error: error.message
+        });
+    }
+});
 
 // DELETE localhost:8080  /api/users /:uid
-router.delete('/:uid', async(req, res)=> {
-    const { uid } = req.params
-
-    const result = await usersModel.deleteOne({_id: uid})
-    res.status(200).send({ 
-        status: 'success', 
-        payload: result })
-})
+router.delete('/:uid', async (req, res) => {
+    try {
+        const { uid } = req.params;
+        if (!uid) {
+            return res.status(400).send({
+                status: 'error',
+                message: 'ID de usuario no proporcionado'
+            });
+        }
+        const result = await usersModel.findByIdAndDelete(uid);
+        if (!result) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'Usuario no encontrado'
+            });
+        }
+        res.status(200).send({
+            status: 'success',
+            payload: result
+        });
+    } catch (error) {
+        console.error(error);
+       
+        res.status(500).send({
+            status: 'error',
+            message: 'Error al procesar la solicitud',
+            error: error.message
+        });
+    }
+});
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const productsRouter = require('./routes/products.router.js')
-const userRouter = require('./routes/users.router.js')
+const usersRouter = require('./routes/users.router.js')
 const viewsRouter = require('./routes/views.router.js')
 const cartsRouter = require('./routes/carts.router.js')
 const {Server} = require('socket.io')
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
-app.use('/api/users,', userRouter)
+app.use('/api/users,', usersRouter)
 
 //MOTOR DE HANDLEBAR//
 app.engine('hbs', handlebars.engine({
@@ -39,28 +39,28 @@ const serverHttp = app.listen(8080, ()=>{
 })
 
 //const socketProduct = (io)=>{}
-//const socketProduct = ()=>{req, res, next}
+const socketProduct = ()=>{req, res, next}
 const socketServer = new Server(serverHttp)
 let arrayMensajes = []
 socketServer.on('connection', socket => {
     console.log('Nuevo cliente conectado')
 
-    //socket.on('recibirMensajeCliente', data => {
-    //    console.log(data)
-    //})
-    //socket.emit('solo-para-el-actual', 'Este lo debe recibir solo el socket actual')
+    socket.on('recibirMensajeCliente', data => {
+        console.log(data)
+    })
+    socket.emit('solo-para-el-actual', 'Este lo debe recibir solo el socket actual')
 
-    //socket.broadcast.emit('para-todos-menos-actual', 'Este evento lo verna todos los conectados, menos el acutal')
+    socket.broadcast.emit('para-todos-menos-actual', 'Este evento lo verna todos los conectados, menos el acutal')
 
-    //socketServer.emit('evento-para-todos', 'este mensaje lo reciben todos')
+    socketServer.emit('evento-para-todos', 'este mensaje lo reciben todos')
 
     
-    //socket.emit('enviar-mensajes-cliente', arrayMensajes)
+    socket.emit('enviar-mensajes-cliente', arrayMensajes)
 
-    //socket.on('message', mensajes =>{
-        //console.log(mensajes)
-      // arrayMensajes.push({id: socket.id, message: mensajes})
-      //socketServer.emit('mensaje-recibido-cliente', arrayMensajes)
-    //})
+    socket.on('message', mensajes =>{
+        console.log(mensajes)
+       arrayMensajes.push({id: socket.id, message: mensajes})
+      socketServer.emit('mensaje-recibido-cliente', arrayMensajes)
+    })
 
 })
